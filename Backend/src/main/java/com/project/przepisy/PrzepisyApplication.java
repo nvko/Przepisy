5aa07f2a308122ba07c38782475b16bd1fa5a28d
Mateusz Ciszewski <matti288422@gmail.com>
2019-03-20 19:44:23 +0100

    backend init

---
+++ Backend/src/main/java/com/project/przepisy/PrzepisyApplication.java
@@ -0,0 +1,13 @@
+package com.project.przepisy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrzepisyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrzepisyApplication.class, args);
+    }
+
+}
